cd516e69628793cca4000c070092b6e061761df2
NathanSachombe <nathanguyo6@gmail.com>
2023-12-07 16:24:12 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -4,5 +4,39 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
+     
+    def __init__(self, name, breed):
+        self.id = None
+        self.name = name
+        self.breed = breed
     
-    pass
+    @classmethod
+    def create_table(self):
+        sql = """"
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+                )
+                """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(self):
+        sql = """
+              DROP TABLE IF EXISTS songs
+              """
+        CURSOR.execute(sql)  
+    
+    def save(self):
+        sql = """
+             INSERT INTO dogs (name, breed)
+             VALUES (?, ?)
+             """
+        CURSOR.execute(sql, (self.name, self.breed))
+
+    
+
+
+    
+
